a4470d9a78f44b7ad777b6dfa90610420495f185
GitHub Action <action@github.com>
2024-04-09 17:43:34 +0000

    Template cleanup

--- src/main/kotlin/com/github/fkorotkov/intellijpluginverifieroverrideonlyapiissue/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/fkorotkov/intellijpluginverifieroverrideonlyapiissue/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.fkorotkov.intellijpluginverifieroverrideonlyapiissue.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.fkorotkov.intellijpluginverifieroverrideonlyapiissue.MyBundle
+import com.github.fkorotkov.intellijpluginverifieroverrideonlyapiissue.services.MyProjectService
 import javax.swing.JButton
 
 
